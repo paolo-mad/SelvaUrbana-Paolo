f58d2e235281f5c728b719a3d2dd75202ec042c3
Paolo Damen <paolodamen@gmail.com>
2024-01-20 18:56:26 +0100

    add elements to product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,26 @@
 const Products = () => {
-    render(
-        <h1>Products</h1>
+    return(
+        <>
+            <div className="container">
+                <header>
+                    <h1>Selva Urbana</h1>
+                    <img src="../assets/logo.png" alt="logo Selva Urbana" />
+                    <img src="" alt="" />
+                </header>
+                <main>
+                    <button>+planta</button>
+                    <ul>
+                        <li>cards</li>
+                    </ul>
+                </main>
+                <footer>
+                    <p>545 553 535</p>
+                    <p>selvaurbana@yahoo.com</p>
+                    <p>©Selva Urbana</p>
+
+                </footer>
+            </div>
+        </>
     )
 }
 
