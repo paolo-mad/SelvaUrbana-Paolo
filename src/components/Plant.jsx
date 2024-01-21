f5316f4fa8ff974757e970f2ea0fcfcd3569e3d7
Paolo Damen <paolodamen@gmail.com>
2024-01-21 18:58:32 +0100

    delete function added

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,5 +1,15 @@
-const Plant = ({plants, handleDelete}) => {
-
+import { useHistory} from 'react'
+const Plant = ({plants}) => {
+    
+    const history = useHistory
+    const handleClick = ()=> {
+        fetch('http://localhost:3001/products/1',{
+            method: 'DELETE'
+        })
+        .then(() => {
+            history.push('./')
+        })
+    }
     return (
         <div className='plant-list'>
             {plants.map((plant) => (
@@ -7,6 +17,7 @@ const Plant = ({plants, handleDelete}) => {
                     <h3 className='cardName'>{plant.name}</h3>
                     <p className="stock">stock {plant.stock}</p>
                     <p className="price">{plant.price} €</p>
+                    <button className="deleteBtn" onClick={handleClick}>delete</button>
                 </div>
             ))}
         </div>
