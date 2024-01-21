5509bb6a958a90f21b50897ba98184cc23291f21
Paolo Damen <paolodamen@gmail.com>
2024-01-21 19:45:46 +0100

    photos added

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -3,20 +3,23 @@ const Plant = ({plants}) => {
     
     const history = useHistory
     const handleClick = ()=> {
-        fetch('http://localhost:3001/products/1',{
+        fetch('http://localhost:3001/products/'+ 1,{
             method: 'DELETE'
         })
         .then(() => {
-            history.push('./')
+            history('./')
         })
     }
     return (
         <div className='plant-list'>
             {plants.map((plant) => (
                 <div className="card" key={plant.id}>
+                    <img src={plant.photo} alt="" className='plantImg'/>
+                    <img src={plant.type} alt="" className='interiorPlant'/>
                     <h3 className='cardName'>{plant.name}</h3>
-                    <p className="stock">stock {plant.stock}</p>
+                    <p className="stock">{plant.stock} <br />stock</p>
                     <p className="price">{plant.price} €</p>
+
                     <button className="deleteBtn" onClick={handleClick}>delete</button>
                 </div>
             ))}
